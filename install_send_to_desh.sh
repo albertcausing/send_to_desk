0b468cfb23254210eef8f4f9c108bce38af8ad78
Alex Dicianu <alex@pantheon.io>
2016-10-13 19:35:43 +0300

    Update install_send_to_desh.sh

--- install_send_to_desh.sh
+++ install_send_to_desh.sh
@@ -16,7 +16,7 @@ echo -n "Please enter your Desk password. This will be stored locally in /usr/lo
 read -s desk_password
 echo
 
-curl -O https://github.com/alexdicianu/send_to_desk/blob/master/send_to_desk
+curl -O https://raw.githubusercontent.com/alexdicianu/send_to_desk/master/send_to_desk
 
 sed -i .txt "s/INTERCOM_APPID/$intercom_app_id/" send_to_desk ; rm send_to_desk.txt
 sed -i .txt "s/INTERCOM_KEY/$intercom_key/" send_to_desk ; rm send_to_desk.txt
