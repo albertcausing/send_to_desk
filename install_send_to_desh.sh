6afb358c62fe3d440a6a4f19ee737d1af1532684
Alex Dicianu <alex@getpantheon.com>
2016-10-13 19:28:27 +0300

    first commit

---
+++ install_send_to_desh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+python -V >/dev/null 2>&1 || { echo >&2 "python is not installed. Aborting."; exit 1; }
+
+
+echo -n "Please enter your Intercom APP ID: "
+read intercom_app_id
+
+echo -n "Please enter your Intercom KEY (input hidden): "
+read -s intercom_key
+echo
+
+echo -n "Please enter your Desk email address: "
+read desk_email
+
+echo -n "Please enter your Desk password. This will be stored locally in /usr/local/bin/send_to_desk (input hidden): "
+read -s desk_password
+echo
+
+sed -i .txt "s/INTERCOM_APPID/$intercom_app_id/" send_to_desk ; rm send_to_desk.txt
+sed -i .txt "s/INTERCOM_KEY/$intercom_key/" send_to_desk ; rm send_to_desk.txt
+sed -i .txt "s/DESK_USER/$desk_email/" send_to_desk ; rm send_to_desk.txt
+sed -i .txt "s/DESK_PASS/$desk_password/" send_to_desk ; rm send_to_desk.txt
+
+echo "Installation complete."
